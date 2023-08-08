a2103c1595df0d856a885b47effc279b9711877e
frontlab <contact@frontlab.kr>
2023-08-09 00:17:32 +0900

    ddd

--- dist/js/common.js
+++ dist/js/common.js
@@ -3,4 +3,12 @@ $(document).ready(function () {
 	$('.datetimepicker').datetimepicker({
 		format: 'L',
 	});
+
+	if ($('.carenation-body .carenation-table').length > 0) {
+		setTimeout(() => {
+			var headHeight = $('.carenation-body .carenation-body__head').outerHeight();
+			console.log(headHeight);
+			$('.carenation-body .carenation-table').css('height', 'calc(100% - ' + headHeight + 'px)');
+		}, 10);
+	}
 });
