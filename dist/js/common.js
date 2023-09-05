5bd41fea73a9805179663a0ab49f0987a64871d3
frontlab <contact@frontlab.kr>
2023-09-05 17:27:04 +0900

    dd

--- dist/js/common.js
+++ dist/js/common.js
@@ -4,11 +4,24 @@ $(document).ready(function () {
 		format: 'L',
 	});
 
+	// Summernote
+	$('#summernote').summernote({
+		height: 200,
+	});
+
+	//scroll
 	if ($('.carenation-body .carenation-table').length > 0) {
 		setTimeout(() => {
 			var headHeight = $('.carenation-body .carenation-body__head').outerHeight();
 			console.log(headHeight);
 			$('.carenation-body .carenation-table').css('height', 'calc(100% - ' + headHeight + 'px)');
+		}, 10);
+	}
+	if ($('.carenation-question').length > 0) {
+		setTimeout(() => {
+			var headHeight = $('.carenation-body .carenation-body__head').outerHeight();
+			console.log(headHeight);
+			$('.carenation-question').css('height', 'calc(100% - ' + headHeight + 'px)');
 		}, 10);
 	}
 });
